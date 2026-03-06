a32e072e62a9380aeb7ed9c049727d37a04fdbb2
rcastanedaf <rcastanedaf@miumg.edu.gt.com>
2026-03-05 23:49:42 -0600

    Get en controlador tipo moneda

---
+++ Controllers/TipoMonedaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Condominio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TipoMonedaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
