92baa9bf29dc316b39c14177d9077513ae411dc8
erick <erickdonis324@gmail.com>
2026-03-11 18:42:17 -0600

    Implementacion de endpoint de parentesco

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddScoped<ITestService, TestService>();
 
 builder.Services.AddScoped<ITestRepository, TestRepository>();
 
+builder.Services.AddScoped<IParentescoService, ParentescoService>();//Mi parte
+builder.Services.AddScoped<IParentescoRepository, ParentescoRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Repositories/Interfaces/IParentescoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Condominio.Models;
+
+namespace Condominio.Repositories.Interfaces
+{
+    public interface IParentescoRepository
+    {
+        public Task<List<ParentescoModel>> GetAll();
+        Task<ParentescoModel> Create(ParentescoModel model);//Para crear un nuevo parentesto
+        Task<ParentescoModel> GetById(int id);//Para bsucar pariente por id
+        Task<ParentescoModel> Update(int id, ParentescoModel model);
+        Task<ParentescoModel> Delete(int id);
+    }
+}
