931f97392c43675d8fa91096f2fa7f30ea2dffdf
Gerson Quiñonez <gersonquionez@gmail.com>
2026-03-11 21:59:08 -0600

    CRUD tipo-contrato backend

--- obj/Debug/net8.0/Condominio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Condominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Condominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680969e6438ff53dc7550ce3e21135a4c9ef09c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e265c1205dafa4421614d196e58202db4fb1cd13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Condominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Condominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
