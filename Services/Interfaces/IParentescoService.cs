e0d6dbb5f1a5d69efa4e416df745c42e8e49d3f2
erick <erickdonis324@gmail.com>
2026-03-20 19:26:23 -0600

    Cambios al backend para funcionar con base de datos

--- Services/Interfaces/IParentescoService.cs
+++ Services/Interfaces/IParentescoService.cs
@@ -5,10 +5,14 @@ namespace Condominio.Services.Interfaces
 {
     public interface IParentescoService
     {
-        Task<List<ParentescoModel>> GetAll();
+        /*Task<List<ParentescoModel>> GetAll();
         Task<ParentescoModel> Create(ParentescoRequest request);
         Task<ParentescoModel> GetById(int id);
         Task<ParentescoModel> Update(int id, ParentescoRequest request);
-        Task<ParentescoModel> Delete(int id);
+        Task<ParentescoModel> Delete(int id);*/
+        public Task<List<ParentescoModel>> GetAll();
+        public Task<ParentescoRequest> Create(ParentescoRequest request);
+        public Task<ParentescoModel> Update(ParentescoModel request, int id);
+        public Task<bool> Delete(int id);
     }
 }
