7bc0107f7a14bd8e087e98e3e514652c2e3f893e
JCermeno-umg <jcermenob@miumg.edu.gt>
2026-04-07 22:52:58 -0600

    Agrego Backend de Residente

---
+++ Models/Residente.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace Condominio.Models
+{
+    public class Residente
+    {
+        public Residente()
+        {
+
+        }
+        public Residente(int idPersona, int idPropiedad, string tipo_Residente, DateOnly fechaIngreso, DateOnly fechaSalida, int estado,
+            string observaciones)
+        {
+            Id_Persona = idPersona;
+            Id_Propiedad = idPropiedad;
+            Tipo_Residente = tipo_Residente;
+            Fecha_Ingreso = fechaIngreso;
+            Fecha_Salida = fechaSalida;
+            Estado = estado;
+            Observaciones = observaciones;
+        }
+        public int Id_Residente { get; set; }
+        public int Id_Persona { get; set; }
+        public int Id_Propiedad { get; set; }
+        public string Tipo_Residente { get; set; }
+        public DateOnly Fecha_Ingreso { get; set; }
+        public DateOnly Fecha_Salida { get; set; }
+        public int Estado { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
---
+++ Services/ResidenteService.cs
@@ -0,0 +1,99 @@
+﻿using Condominio.DTOs.Request;
+using Condominio.Models;
+using Condominio.Services.Interfaces;
+
+namespace Condominio.Services
+{
+    public class ResidenteService : IResidenteService
+    {
+        private readonly IResidenteService _residenteRepository;
+        public ResidenteService(IResidenteService residenteRepository)
+        {
+            _residenteRepository = residenteRepository;
+        }
+
+        public async Task<List<Residente>> GetAllAsync()
+        {
+            try
+            {
+                var allResidente = await _residenteRepository.GetAllAsync();
+
+                return allResidente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Residente>> GetId(int id)
+        {
+            try
+            {
+                var idResidente = await _residenteRepository.GetId(id);
+
+                return idResidente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Residente>> GetNombre(string nombre)
+        {
+            try
+            {
+                var nombreResidente = await _residenteRepository.GetNombre(nombre);
+
+                return nombreResidente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<ResidenteUpdateRequest> UpdateResidente(ResidenteUpdateRequest editResidente)
+        {
+            try
+            {
+                var residente = await _residenteRepository.UpdateResidente(editResidente);
+
+                return residente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<ResidenteCreateRequest> CreateResidente(ResidenteCreateRequest newResidente)
+        {
+            try
+            {
+                var residente = await _residenteRepository.CreateResidente(newResidente);
+
+                return residente;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> DeleteResidente(int id)
+        {
+            try
+            {
+                var response = _residenteRepository.DeleteResidente(id);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
