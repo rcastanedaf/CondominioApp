2e3b30d69b923ddf9063a1a7bb408343117787b8
JCermeno-umg <jcermenob@miumg.edu.gt>
2026-04-07 23:44:32 -0600

    Agrego Backend de Tipo Propiedad

---
+++ DTOs/Request/TipoPropiedadUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Condominio.DTOs.Request
+{
+    public class TipoPropiedadUpdateRequest
+    {
+        public required int Id_Tipo_Propiedad { get; set; }
+        public required string Nombre { get; set; }
+        public required string Descripcion { get; set; }
+    }
+}
---
+++ Services/TipoPropiedadService.cs
@@ -0,0 +1,99 @@
+﻿using Condominio.DTOs.Request;
+using Condominio.Models;
+using Condominio.Services.Interfaces;
+
+namespace Condominio.Services
+{
+    public class TipoPropiedadService : ITipoPropiedadService
+    {
+        private readonly ITipoPropiedadService _tipoPropiedadRepository;
+        public TipoPropiedadService(ITipoPropiedadService tipoPropiedadRepository)
+        {
+            _tipoPropiedadRepository = tipoPropiedadRepository;
+        }
+
+        public async Task<List<Tipo_Propiedad>> GetAllAsync()
+        {
+            try
+            {
+                var allTipoPropiedad = await _tipoPropiedadRepository.GetAllAsync();
+
+                return allTipoPropiedad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Tipo_Propiedad>> GetId(int id)
+        {
+            try
+            {
+                var idTipoPropiedad = await _tipoPropiedadRepository.GetId(id);
+
+                return idTipoPropiedad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Tipo_Propiedad>> GetNombre(string nombre)
+        {
+            try
+            {
+                var nombreTipoPropiedad = await _tipoPropiedadRepository.GetNombre(nombre);
+
+                return nombreTipoPropiedad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<TipoPropiedadUpdateRequest> UpdateTipoPropiedad(TipoPropiedadUpdateRequest editTipoPropiedad)
+        {
+            try
+            {
+                var tipoPropiedad = await _tipoPropiedadRepository.UpdateTipoPropiedad(editTipoPropiedad);
+
+                return tipoPropiedad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<TipoPropiedadCreateRequest> CreateTipoPropiedad(TipoPropiedadCreateRequest newTipoPropiedad)
+        {
+            try
+            {
+                var tipoPropiedad = await _tipoPropiedadRepository.CreateTipoPropiedad(newTipoPropiedad);
+
+                return tipoPropiedad;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> DeleteTipoPropiedad(int id)
+        {
+            try
+            {
+                var response = _tipoPropiedadRepository.DeleteTipoPropiedad(id);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
