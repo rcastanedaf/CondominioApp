2f716992423c1afe8bbb1a5ff58822e1d5fe9f1e
rcastanedaf <rcastanedaf@miumg.edu.gt.com>
2026-04-11 03:05:51 -0600

    correcciones

--- DTOs/Request/MultaUpdateRequest.cs
+++ DTOs/Request/MultaUpdateRequest.cs
@@ -5,18 +5,16 @@
         public required int Id_Multa { get; set; }
         public required int Id_Residente { get; set; }
         public required int Id_Propiedad { get; set; }
-        public required int Id_Tipo_Infraccion { get; set; }
         public required string Descripcion { get; set; }
         public required decimal Monto { get; set; }
-        public required DateOnly Fecha_Infraccion { get; set; }
-        public required DateOnly Fecha_Vencimiento { get; set; }
-        public required int Estado { get; set; }
-        public required string Evidencia { get; set; }
-        public required int Id_Factura { get; set; }
-        public required int Id_Apelacion { get; set; }
-        public required int Id_Emitida { get; set; }
-        public required int Id_Aprobada { get; set; }
+        public required DateTime Fecha_Infraccion { get; set; }
+        public required DateTime Fecha_Vencimiento { get; set; }
+        public required string Estado { get; set; }
+        public string? Evidencia { get; set; }
+        public int? Id_Factura { get; set; }
+        public int? Id_Apelacion { get; set; }
+        public int? Id_Emitida { get; set; }
+        public int? Id_Aprobada { get; set; }
         public required string Observaciones { get; set; }
-        public required DateOnly Fecha_Registro { get; set; }
     }
 }
